354aa16d27d4c2cf3e815120f3d082dde5297555
ifeanyiobi <obiguy928@gmail.com>
2023-12-16 09:13:56 -0800

    printing lower case letter

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry stage
+ * Return:succesful  whenever it is 0
+ */
+int main(void)
+{
+	char c;
+	c = 'a';
+	while
+	       	(c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+		return(0);
+}
